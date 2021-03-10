98f6532b23c11195c092ef2029939bef2aea8dad
Breno Duque <breno.ga.duque@gmail.com>
2021-03-10 02:31:39 -0300

    Comecando estudos com estrutura de dados

---
+++ NotacaoBigO/Cap1.py
@@ -0,0 +1,15 @@
+#633ns por loop
+def soma(n):
+    soma = 0 
+    for x in range(n+1):
+        soma+= x
+
+    return print(soma)
+
+soma(10)
+
+#116ns por loop
+def soma2(n):
+    return print((n * (n + 1)) / 2)
+
+soma2(10)
