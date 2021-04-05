5ee2556e17333a61d05e790545ee04cbfba90b19
Breno <breno.ga.duque@gmail.com>
2021-04-05 02:18:59 -0300

    implementado o magick type

--- NotacaoBigO/Cap1.py
+++ NotacaoBigO/Cap1.py
@@ -1,15 +1,32 @@
-#633ns por loop
+# %%
 def soma(n):
     soma = 0
     for x in range(n+1):
         soma+= x
 
-    return print(soma)
+    return soma
 
-soma(10)
+%timeit soma(10)
 
-#116ns por loop
+# %%
 def soma2(n):
-    return print((n * (n + 1)) / 2)
+    return (n * (n + 1)) / 2
 
-soma2(10)
+%timeit soma2(10)
+
+# %%
+def lista1():
+    lista = []
+    for i in range(1000):
+        lista += [i]
+    return lista
+
+%timeit lista1()
+
+# %%
+
+def lista2():
+    return range(1000)
+
+%timeit lista2()
+
